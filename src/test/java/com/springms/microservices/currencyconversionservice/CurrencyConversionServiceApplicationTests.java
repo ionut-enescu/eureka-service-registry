88f081086e6c6ec328bd19e7897105869871e34e
Ionut Enescu <ionut.462.enescu@gmail.com>
2022-09-08 00:01:44 +0300

    from spring.io

---
+++ src/test/java/com/springms/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springms.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
